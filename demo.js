bc75af69d9cacb791bc7e84de2c2e9c851c7c7db
liyifei <liyifei@le.com>
2017-03-01 18:14:18 +0800

    update demo

--- demo.js
+++ demo.js
@@ -3,5 +3,16 @@
  * @authors liyifei
  * @date 2017/02
  */
-var a = 1;
-console.log(a);
+
+// 禁止出现未使用过的变量（不检查参数）
+// good
+var foo = 1;
+function doSomething() {
+    return foo;
+}
+doSomething();
+// bad
+// var foo = 1;
+// function doSomething() {}
+
+// 禁止在嵌套的块中出现变量声明或 function 声明
